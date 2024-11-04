0b397c7737ae9b2b116abefcf3dfcf3429b6a926
Shir <shirturgeman@campus.technion.ac.il>
2024-11-04 08:16:21 +0200

    4/11 8:15 new searches and heuristics

--- search/Astar.py
+++ search/Astar.py
@@ -1,43 +1,20 @@
 from queue import PriorityQueue
-from typing import Callable, List, Tuple, Dict
+from typing import Callable, List, Tuple
 from CARRI.realm import State  # Assuming this manages the problem state
 from CARRI.action import Step  # Assuming steps/actions are defined in this module
 import time
 import logging
+from CARRI.action import Step, ActionStringRepresentor  # Defines the possible actions or steps that can be taken in the problem domain
 from CARRI.simulator import Simulator
 
-def reconstruct_path(simulator, came_from: Dict[State, State], current: State) -> List[Step]:
-    """
-    Reconstructs the path from the start state to the given state.
-    :param came_from: Dictionary that maps each state to its predecessor.
-    :param current: The current state from which to reconstruct the path.
-    :return: The sequence of steps (actions) leading to the given state.
-    """
-    total_path = []
-    while current in came_from:
-        previous, action = came_from[current]
-
-        act = []
-        for a in action:
-            x = simulator.actionStringRepresentor.represent(a)
-            act.append(x)
-
-        total_path.insert(0, act)  # Prepend the action to the total path
-        current = previous
-
-    i = 0
-    for t in total_path:
-        print(f' {i}. {t}')
-        i += 1
-    return total_path
-
 def a_star_search(simulator: Simulator,
-                  heuristic: Heuristic,
+                  heuristic: Callable[[State], float],
                   time_limit: float) -> Tuple[List[Step], State]:
     """
     A* search algorithm with flexible heuristic input.
-
+    
     Args:
+    - initial_state: The starting state of the problem.
     - simulator: An instance of CARRISimulator to generate successors.
     - heuristic: A heuristic function that estimates the cost to reach the goal from a given state.
     - time_limit: The maximum time allowed for this search.
@@ -46,49 +23,55 @@ def a_star_search(simulator: Simulator,
     - A tuple of (List of Steps, final state). Returns the best partial solution found within the time limit.
     """
     start_time = time.time()
+    frontier = PriorityQueue()
+    initial_state = simulator.current_state
+    frontier.put((0, initial_state))
+    
+    g_costs = {initial_state: 0}
+    came_from = {initial_state : (0, 0)}
+    best_state = initial_state
+    best_path = []
 
-    # The open set containing discovered nodes that may need to be (re-)expanded
-    open_set = PriorityQueue()
-    open_set.put((heuristic(simulator.current_state), simulator.current_state))
+    i = 0 
 
-    # Dictionary to reconstruct the path
-    came_from: Dict[State, Tuple[State, Step]] = {}
-
-    # Cost from start to a given node
-    g_score = {simulator.current_state: 0}
-
-    while not open_set.empty() and (time.time() - start_time) < time_limit:
-        # Get the node from open set with the lowest f_score value
-        _, current_state = open_set.get()
-
-        # Check if we reached the goal state
-        if all_packages_delivered(current_state):
-            return reconstruct_path(simulator, came_from, current_state), current_state
-    while not open_set.empty() and (time.time() - start_time) < time_limit:
-        # Get the node from open set with the lowest f_score value
-        _, current_state = open_set.get()
-
-        # Check if we reached the goal state
-        if all_packages_delivered(current_state):
-            return reconstruct_path(simulator, came_from, current_state), current_state
-
-        # Generate successors of the current state
-        #x = simulator.generate_successors(current_state.copy())
-        for next_state, action, cost in simulator.generate_successors(current_state):
-            tentative_g_score = g_score[current_state] + cost
-
-            # If we found a better path to the next_state
-            if next_state not in g_score or tentative_g_score < g_score[next_state]:
-                came_from[next_state] = (current_state, action)
-                g_score[next_state] = tentative_g_score
-                f_score = tentative_g_score + heuristic.evaluate(next_state)
-                open_set.put((f_score, next_state))
+    while not frontier.empty() and (time.time() - start_time) < time_limit:
+        _, current_state = frontier.get()
+        pre_state, action = came_from[current_state]
+        if action != 0:
+            print(f'iter {i}:')
+            i+= 1
+            for a in action:
+                print(simulator.actionStringRepresentor.represent(a))
         
-        print("\n\nso far :")
-        reconstruct_path(simulator, came_from, current_state)
+        if all_packages_delivered(current_state):  # Goal condition: all packages delivered
+            return reconstruct_path(came_from, current_state), current_state
 
-    # If open set is empty or time limit is exceeded, return the best path found so far
-    return reconstruct_path(simulator, came_from, current_state), current_state
+        for next_state, action, cost in simulator.generate_successor_states(current_state):
+            new_g_cost = g_costs[current_state] + cost
+            
+            if next_state not in g_costs or new_g_cost < g_costs[next_state]:
+                g_costs[next_state] = new_g_cost
+                f_cost = new_g_cost + heuristic.evaluate(next_state)
+                frontier.put((f_cost, next_state))
+                came_from[next_state] = (current_state, action)
+
+                # Update the best state and path if we find a better partial solution
+                if heuristic.evaluate(next_state) < heuristic.evaluate(best_state):
+                    best_state = next_state
+                    best_path = reconstruct_path(came_from, next_state)
+
+    logging.warning("A* search terminated without finding a complete solution within the time limit.")
+    return best_path, best_state
+
+def reconstruct_path(came_from, end_state):
+    path = []
+    current = end_state
+    while current in came_from:
+        previous, action = came_from[current]
+        path.append(action)
+        current = previous
+    path.reverse()
+    return path
 
 def all_packages_delivered(state: State) -> bool:
     """
@@ -97,4 +80,6 @@ def all_packages_delivered(state: State) -> bool:
     :return: True if all packages are delivered, False otherwise.
     """
     # Assuming `state` has a method or attribute to check the status of packages
+    x = len(state.items[0]) == 0
     return len(state.items[0]) == 0
+   
